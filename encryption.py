68d154b9fa52b8c78c14b4f0d6c470d53b924229
Calvin Pelletier <calvin.pelletier@gmail.com>
2017-03-28 15:05:02 -0500

    added untested encryption code

--- encryption.py
+++ encryption.py
@@ -9,27 +9,24 @@ import shutil
 
 USER = getpass.getuser()
 HOME_PATH = '/home/' + USER
-PUBLIC_KEY_PATH = ''
-PRIVATE_KEY_PATH = ''
-ENCRYPTED_KEY_PATH = ''
 
 def encrypt_home(password):
-    public_key, private_key = gen_key_rsa(password)
-    with open(PUBLIC_KEY_PATH, 'w') as f:
-        f.write(public_key)
-    with open(PRIVATE_KEY_PATH, 'w') as f:
-        f.write(private_key)
+    # public_key, private_key = gen_key_rsa(password)
+    # with open(PUBLIC_KEY_PATH, 'w') as f:
+    #     f.write(public_key)
+    # with open(PRIVATE_KEY_PATH, 'w') as f:
+    #     f.write(private_key)
     subprocess.check_output('zip -r {0}.zip {0}'.format(HOME_PATH), shell=True)
     with open(HOME_PATH + '.zip', 'r') as f:
         zip_data = f.read()
     iv = Random.new().read(AES.block_size)
-    key = gen_key_aes()
+    key = gen_key_aes(password)
     encrypted_data = iv + encrypt_aes(zip_data, key, iv)
-    encrypted_key = encrypt_rsa(key, public_key)[0]
+    # encrypted_key = encrypt_rsa(key, public_key)[0]
     with open(HOME_PATH + '.locked', 'w') as f:
         f.write(encrypted_data)
-    with open(ENCRYPTED_KEY_PATH, 'w') as f:
-        f.write(encrypted_key)
+    # with open(ENCRYPTED_KEY_PATH, 'w') as f:
+    #     f.write(encrypted_key)
     os.remove(HOME_PATH + '.zip')
     shutil.rmtree(HOME_PATH)
 
@@ -37,17 +34,18 @@ def decrypt_home(password):
     if not os.path.isfile(HOME_PATH + '.locked'):
         print 'Home is already decrypted.'
         return
-    with open(ENCRYPTED_KEY_PATH, 'r') as f:
-        encrypted_key = f.read()
-    with open(PRIVATE_KEY_PATH, 'r') as f:
-        private_key = f.read()
-    with open(HOME_PATH + '.locked', 'r') as f:
-        iv = f.read(AES.block_size)
-        encrypted_data = f.read()
-    try:
-        key = decrypt_rsa(encrypted_key, private_key, SHA256.new(password).hexdigest())
-    except ValueError:
-        print 'Incorrect password.'
+    # with open(ENCRYPTED_KEY_PATH, 'r') as f:
+    #     encrypted_key = f.read()
+    # with open(PRIVATE_KEY_PATH, 'r') as f:
+    #     private_key = f.read()
+    # with open(HOME_PATH + '.locked', 'r') as f:
+    #     iv = f.read(AES.block_size)
+    #     encrypted_data = f.read()
+    # try:
+    #     key = decrypt_rsa(encrypted_key, private_key, SHA256.new(password).hexdigest())
+    # except ValueError:
+    #     print 'Incorrect password.'
+    key = gen_key_aes(password)
     zip_data = decrypt_aes(encrypted_data, key, iv)
     with open(HOME_PATH + '.zip', 'w') as f:
         f.write(zip_data)
@@ -56,11 +54,11 @@ def decrypt_home(password):
     os.remove(HOME_PATH + '.zip')
 
 
-def encrypt_rsa(data, key):
-    return RSA.importKey(key).encrypt(data, 0)
+# def encrypt_rsa(data, key):
+#     return RSA.importKey(key).encrypt(data, 0)
 
-def decrypt_rsa(data, key, password_hash):
-    return RSA.importKey(key, password_hash).decrypt(data)
+# def decrypt_rsa(data, key, password_hash):
+#     return RSA.importKey(key, password_hash).decrypt(data)
 
 def encrypt_aes(data, key, iv):
     data = data.rstrip('\n')
@@ -73,10 +71,10 @@ def encrypt_aes(data, key, iv):
 def decrypt_aes(data, key, iv):
     return AES.new(key, AES.MODE_CBC, iv).decrypt(data).rstrip('\n')
 
-def gen_key_rsa(password):
-    private_key = RSA.generate(2048)
-    public_key = private_key.publickey()
-    return public_key.exportKey(), private_key.exportKey(passphrase=SHA256.new(password).hexdigest())
+# def gen_key_rsa(password):
+#     private_key = RSA.generate(2048)
+#     public_key = private_key.publickey()
+#     return public_key.exportKey(), private_key.exportKey(passphrase=SHA256.new(password).hexdigest())
 
 def gen_key_aes(password=None):
     if password is None:
